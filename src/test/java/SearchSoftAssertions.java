5309ff6b101949a156e689ca8bf33898d8b2d8c9
Sarycheva Daria <d.saricheva@gmail.com>
2024-09-12 10:08:52 +0300

    added searchSoftAssertionsTestEdit

--- src/test/java/SearchSoftAssertions.java
+++ src/test/java/SearchSoftAssertions.java
@@ -1,4 +1,3 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class searchSoftAssertions {
+public class SearchSoftAssertions {
 
     @BeforeAll
     static void beforeAll() {
@@ -21,21 +20,23 @@ class searchSoftAssertions {
 
     @Test
     void softAssertionsTest() {
-        //открыть страницу Selenide в Github
-        open("/selenide/selenide");
-        //Перейти в раздел Wiki проект
-        $("#wiki-tab").click();
-        //Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
-        // 1 вариант
-        //$("#wiki-pages-filter").setValue("SoftAssertions"); // В строку поиска по страницам ввести SoftAssertions
-        //$("#wiki-pages-box").shouldHave(text("SoftAssertions")); //проверка результата поиска
-        // 2 вариант
-        $(".Box-row wiki-more-pages-link").$("button").click(); // нажать "Show 3 more pages…"
-        $("#wiki-pages-box").shouldHave(text("SoftAssertions")); //проверка: SoftAssertions есть на странице
-        //Открыть страницу SoftAssertions/Клик по ссылке
-        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        //Проверить, что внутри есть пример кода для JUnit5
-        $("#wiki-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class}) class Tests { @Test void test() { Configuration.assertionMode = SOFT; open(\"page.html\");  $(\"#first\").should(visible).click(); $(\"#second\").should(visible).click(); } }"));
-
+        open("/selenide/selenide"); //открыть страницу
+        $("#wiki-tab").click(); //Перейти в раздел Wiki
+        $(".js-wiki-more-pages-link").click(); // нажать "Show 3 more pages…"
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click(); //Открыть страницу SoftAssertions
+        $(".markdown-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
     }
 }
